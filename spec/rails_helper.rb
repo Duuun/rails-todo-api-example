eb5eb4091669517b1bc0f68b9ed5e4d3364af803
Duuun <daimura10@gmail.com>
2018-12-12 21:30:40 +0900

    Added rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,22 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-# require database cleaner at the top level
-require 'database_cleaner'
-
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+# require database cleaner at the top level
+require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -84,5 +75,13 @@ RSpec.configure do |config|
     DatabaseCleaner.cleaning do
       example.run
     end
+  end
+end
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,5 @@
+require 'rails-helper'
+
+Rspec.describe 'Todos API', type: :request do
+  
+end
