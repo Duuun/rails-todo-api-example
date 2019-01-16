638d6b80b9e1db833dac192d4eb965d54797626a
Duuun <daimura10@gmail.com>
2019-01-17 02:19:42 +0900

    modified api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, { format: 'json' } do
-    resources :todos do
-      resources :items
+    namespace :v1 do
+      resources :todos do
+        resources :items
+      end
     end
   end
 end
